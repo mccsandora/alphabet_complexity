47efa5a486cc97a827fd083af59c56c59af52723
juliaespositon <97986817+juliaespositon@users.noreply.github.com>
2023-06-27 13:02:27 -0400

    Update complexity_metrics.py

--- utils/complexity_metrics.py
+++ utils/complexity_metrics.py
@@ -5,3 +5,15 @@ def pixel_count_complexity(picture):
     Computes fraction of black pixels in image
     """
     return np.mean(picture)
+
+
+def check_symmetry(picture):
+    symm = 0
+    for j in range(len(picture)):   
+        for i in range(int(np.floor(len(picture[0])/2))):
+            if picture[j][i] == picture[j][-i]:
+                   symm += 1
+            else: symm += 0
+    try: symm /= (len(picture)*(len(picture[0])/2))
+    except: symm = 0
+    return symm
