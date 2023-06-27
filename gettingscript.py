80a1007a2d17137b066ed62d5569ca7b83c3418a
juliaespositon <97986817+juliaespositon@users.noreply.github.com>
2023-06-27 09:44:51 -0400

    Update gettingscript.py

--- gettingscript.py
+++ gettingscript.py
@@ -15,22 +15,25 @@ def get_script(note):
             pass
     return scr
 
-#returned array does not include variants of scripts mentioned in Anna's, just the base script name
-def scriptcomp():
-    scriptsdf = pd.read_csv('data/scriptlist.csv')
-    scripts = list(scriptsdf.Script)
-    annascriptsdf = pd.read_csv('data/annascripts.csv')
-    annascr = annascriptsdf.to_dict('list')
+#variants = True returns a list that includes all variant names from Anna's list (since some of the scripts would have a base name, and then other versions) in the merge
+#variants = False returns just a list of the base name scripts from both lists merged together
 
-    annalist = annascr['EnglishName']
-    for i in range(len(annalist)):
-        annalist[i] = annalist[i].upper() 
+def scriptcomp(variants = True):
+    scripts = list(pd.read_csv('data/scriptlist.csv').Script)
+    ascripts = pd.read_csv('data/annascripts.csv').to_dict('list')
+    alist = ascripts['EnglishName']
+    alist = [k.upper() for k in alist]
+    if variants == False:
+        matchscr = [get_script(i) for i in annalist]
+        a_only = np.asarray(annalist)[np.where(np.asarray(matchscr) == 'None')[0]]
 
-    matchscr = [get_script(i) for i in annalist]
-    a_only = np.asarray(annalist)[np.where(np.asarray(matchscr) == 'None')[0]]
-
-    print('Length of Anna\'s Scripts : ' + str(len(annalist)))
-    print('Matching Script Count : ' + str(len(np.unique(matchscr))-1))
-    print('Length of Extra Scripts in Anna\'s : ' + str(len(np.where(np.asarray(matchscr) == 'None')[0])))
-    
-    return np.concatenate((np.asarray(scripts, dtype = object), a_only))
+        print('Length of Anna\'s Scripts : ' + str(len(annalist)))
+        print('Matching Script Count : ' + str(len(np.unique(matchscr))-1))
+        print('Length of Extra Scripts in Anna\'s : ' + str(len(np.where(np.asarray(matchscr) == 'None')[0])))
+        
+        return np.concatenate((np.asarray(scripts, dtype = object), a_only))
+    else:
+        scripts = set(scripts)
+        alist = set(alist)
+        newscr = alist - scripts
+        return list(scripts) + list(newscr)
